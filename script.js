323796d13442dd050d98323e4af359080077f4ac
Jaden Stanton <68559776+Jadenstanton@users.noreply.github.com>
2022-10-12 20:48:10 -0500

    Finishing hashing algos

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
 function hashFunc() {
-    var hash = 'empty';
-    let i = 0;
-    var str = document.getElementById("text").value;
+    var str = document.getElementById("password").value;
 
-    var s = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
-
-    for(i = 0; i<str.length;i++){
-        // if(str[i] % 3 == 1){
-        //     str[i] = s[i%3 + 1];
-        // }
-        str[i] = 'a';
+    var s = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+    
+    var alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', "-", "_", ".", "&","?", "!", "@", "#", "/"];
+    var alphabets13 = ['N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M', "-", "_", ".", "&","?", "!", "@", "#", "/"];
+   
+    var newStr = [...str];
+    var myHash = 0;
+    var char = str;
+  
+    for(let i = 0; i<newStr.length;i++){
+        newStr[i] = char;
+        char = str.charCodeAt(0);
+        myHash = ((myHash << 5) - myHash) + char;
+        myHash = myHash & myHash;
+        newStr[i] = myHash + s[i + 10 % 3 +2] ;
     }
-    hash = str;
+    console.log("New string: ", newStr.join(""));
+    var hash = newStr.join("");
 
-    document.getElementById("password").innerHTML = hash;
-    console.log("successful")
+    document.getElementById("hashedPassword").innerHTML = hash;
+    console.log("Successful: ", hash);
 }
 
 function hashSalt() {
-    var str = document.getElementById("pwd").value;
+    var str = document.getElementById("password").value;
+
+    const array = new Uint32Array(1);
+    self.crypto.getRandomValues(array);
+
+    console.log("Salt:", array[0]);
+    
+    var s = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+    
+    var salt = array[0];
+    salt = salt.toString().split('').map(Number).map(n => (n || 10) + 64).map(c => String.fromCharCode(c)).join('');
+
+
+    var newStr = [...str+salt];
+    console.log("Before hash: ", newStr);
+
+    var myHash = 0;
+    var char = str;
+  
+    for(let i = 0; i<newStr.length;i++){
+        newStr[i] = char;
+        char = str.charCodeAt(0);
+        myHash = ((myHash << 5) - myHash) + char;
+        myHash = myHash & myHash;
+        newStr[i] = myHash + s[i + 10 % 3 +2] ;
+    }
+
+    var hash = Array.isArray(newStr) ? newStr.join("") : "";
+    console.log("After hash: ", hash);
+
+
+    document.getElementById("hashedPassword").innerHTML = hash;
+    console.log("Successful: ", hash);
 }
